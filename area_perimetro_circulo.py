8f9e54033918aa8120a66cd17ce71dd0026ada1c
AlfonzoDebian <luismarquez@colegioguanenta.edu.co>
2025-01-29 07:05:09 -0500

    first7

---
+++ area_perimetro_circulo.py
@@ -0,0 +1,20 @@
+import math
+print("────୨ৎ────")
+print("Área y perímetro del círculo >ᴗ<")
+print("────୨ৎ────")
+
+# Input
+print("────୨ৎ────")
+r = float(input(" >ᴗ< Digite el valor del radio: "))
+print("────୨ৎ────")
+
+# Processing
+P = 2 * math.pi * r
+A = math.pi * r * r
+
+# Output 
+print("────୨ৎ────")
+print(" ☪︎ El perímetro es: " + str(P))
+print("────୨ৎ────")
+print(" ☪︎ El área es: " + str(A))
+print("────୨ৎ────")
